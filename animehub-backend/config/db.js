1b328ec2d026b56b1b742d835ca7156f9b410149
Zhiyuan <songzhiyuan98@gmail.com>
2024-10-13 14:46:18 -0700

    update env

--- animehub-backend/config/db.js
+++ animehub-backend/config/db.js
@@ -1,13 +1,14 @@
 //负责数据库连接配置
 //功能：连接到 MongoDB 数据库，并在控制台输出连接成功或失败的信息。
-const mongoose = require('mongoose'); //导入moogodb数据库
+const mongoose = require("mongoose"); //导入moogodb数据库
+require("dotenv").config(); //导入环境变量配置
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/animehub'); //异步请求等待数据库连接
-    console.log('MongoDB connected'); //返回成功响应
+    await mongoose.connect(process.env.MONGO_URI); //异步请求等待数据库连接
+    console.log("MongoDB connected"); //返回成功响应
   } catch (err) {
-    console.log('MongoDB connection error: ', err); //返回错误响应
+    console.log("MongoDB connection error: ", err); //返回错误响应
   }
 };
 
